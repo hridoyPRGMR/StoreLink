260f6e459d6374385d6f446c9f63ead53d188339
hridoyPRGMR <hridoymia.prgmr@gmail.com>
2025-03-04 20:55:56 +0600

    Added updateProduct api,fixed add product api -for shop

--- src/main/java/com/storelink/repository/ShopProductRepository.java
+++ src/main/java/com/storelink/repository/ShopProductRepository.java
@@ -1,5 +1,7 @@
 package com.storelink.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.storelink.model.ShopProduct;
 
 @Repository
 public interface ShopProductRepository extends JpaRepository<ShopProduct, Long> {
+
+	ShopProduct findByShopIdAndAttributeId(long shopId, long attributeId);
+
+	Set<ShopProduct> findByShopId(long shopId);
 	
 }
